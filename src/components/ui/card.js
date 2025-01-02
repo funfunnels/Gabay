44cd975fafa45ca404e084ed919cb69e80c8ced4
funfunnels <117922680+funfunnels@users.noreply.github.com>
2025-01-02 15:36:41 +0200

    Delete src/components/ui directory

--- src/components/ui/card.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const Card = ({ children }) => <div className="card">{children}</div>;
-export const CardHeader = ({ children }) => <div className="card-header">{children}</div>;
-export const CardTitle = ({ children }) => <h3 className="card-title">{children}</h3>;
-export const CardContent = ({ children }) => <div className="card-content">{children}</div>;
